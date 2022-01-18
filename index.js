971e7cde5f3553770d41388bf8ec2e378a356dff
elsie-dev <elcymao@gmail.com>
2022-01-18 00:13:26 -0800

    adding popup button for the signup page

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-let src = ['images/alcohol.jpg','images/detegent.jpg','images/fashion.jpg','images/home.jpg','images/phone.jpg','images/shoe.jpg'];
-
-//constants
-const img = document.createElement('img');
-const dropdown = document.getElementsByClassName('dropdown');
-
-setInterval(function(){
-    img.setAttribute('src',src[Math.floor(Math.random()*src.length)])
-    document.querySelector('.categoryresults').appendChild(img);
-},5000)
-
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
